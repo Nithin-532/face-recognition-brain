8af1efd0e9bec87f095e2fbc97753a7f18802f56
Nithin Kalluru <kallurunithin8771@gmail.com>
2023-06-21 12:33:21 +0530

    backend connected

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3001/signin', {
+    fetch('https://mybackend-edwf.onrender.com/signin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
